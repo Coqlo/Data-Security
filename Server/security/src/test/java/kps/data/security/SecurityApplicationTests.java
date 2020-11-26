aa02b28b7a9f4a56773b0730f7a2a5052cdaa7ed
Aun <ihearaun@gmail.com>
2020-11-26 19:13:05 +0700

    Added

---
+++ Server/security/src/test/java/kps/data/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package kps.data.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
